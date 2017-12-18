a3032e34873709faa06a9dda5fd9f3e17944ab7b
NoobWebAcmen <578071052@qq.com>
2017-12-18 16:10:18 +0800

    2017/12/18 /1610 saved

--- canvas_map/bullet.php
+++ canvas_map/bullet.php
@@ -1,12 +1,173 @@
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<html class="no-js">
     
-</body>
+    <head>
+    <!-- <meta charset="UTF-8"> -->
+        <title>CyberManagerV1.0</title>
+        
+        <!-- Bootstrap -->
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="../vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">
+        <link href="../images/cyber.ico" rel="shortcut icon" />
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script src="../vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <script src="../vendors/xlsx.full.min.js"></script>   
+        <link href="../assets/styles.css" rel="stylesheet" media="screen">
+        
+    </head>
+    <?php  
+        require_once '../cookies/common.php';
+        checkUserValidate();
+        
+
+    ?> 
+    
+    <body>
+    
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="../index/index.php">Cyber后台管理系统V1.0</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav pull-right">
+                            <li class="dropdown">
+                                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown" id="whatever"> <i class="icon-user"></i><?php showUserName();?><i class="caret"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a tabindex="-1" href="#">个人资料</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a tabindex="-1" href="#">修改密码</a>
+                                    </li>
+                                    <li>
+                                        <a tabindex="-1" href="../login/logout.php">切换账号</a>
+                                    </li>
+                                    <li>
+                                        <a tabindex="-1" href="../login/logout.php">退出登录</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <ul class="nav">
+                            <li class="active">
+                                <a href="#">主页</a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">功能 <b class="caret"></b>
+
+                                </a>
+                                <ul class="dropdown-menu" id="menu1">
+                                    <li>
+                                        <a href="#">Tools <i class="icon-arrow-right"></i>
+
+                                        </a>
+                                        <ul class="dropdown-menu sub-menu">
+                                            <li>
+                                                <a href="#">Reports</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Logs</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Errors</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#">SEO Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Other Link</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a href="#">Other Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Other Link</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">扩展应用 <i class="caret"></i>
+
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a tabindex="-1" href="#">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a tabindex="-1" href="#">News</a>
+                                    </li>
+                                    <li>
+                                        <a tabindex="-1" href="#">Custom Pages</a>
+                                    </li>
+                                    <li>
+                                        <a tabindex="-1" href="#">Calendar</a>
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a tabindex="-1" href="#">FAQ</a>
+                                    </li>
+                                </ul>
+                            </li>
+                         
+                    </div>
+                    <!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid map"> 
+                <!--/span-->
+                <div class="span9 map_width" id="content">
+                    <div class="row-fluid clear">
+                            <div class ="bulletControlBar clear fl">
+                                <div class="grade_warp">  
+                                    <div class="User_ratings User_grade">  
+                                        <div class="ratings_bars">  
+                                            <span id="title0">0</span>  
+                                            <span class="bars_10">0</span>  
+                                            <div class="scale" id="bar0">  
+                                                <div></div>  
+                                                <span id="btn0"></span>  
+                                            </div>  
+                                            <span class="bars_10">10</span>  
+                                        </div>  
+                                    </div>  
+                                </div>
+                            </div>
+                            <div class="canvas_div pull-left">
+                                <canvas id="canvas_main" width="860px" height="500px"  >
+                                    <span>不支持canvans的浏览器</span>
+                                </canvas>
+                            </div>
+                    </div>
+                </div>
+        </div>
+
+    </div>
+        <!--/.fluid-container-->
+        
+        <script type="text/javascript" src="cavas_js/jCanvaScript.1.5.18.min.js"></script>
+        <script src="../vendors/jquery-1.9.1.min.js"></script>
+        <script src="../assets/zxx.drag.1.0-min.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+        <script src="../vendors/easypiechart/jquery.easy-pie-chart.js"></script>
+        <script src="../assets/scripts.js"></script>
+        
+        
+    </body>
 </html>
+
+
